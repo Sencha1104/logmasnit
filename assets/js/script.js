81a55f9b41943fc1250926213e2d0e250e65a8ff
せんちゃ <Senchar1104@gmail.com>
2025-10-12 07:22:44 +0900

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+document.querySelectorAll(".folder-button").forEach(button => {
+    button.addEventListener("click", () => {
+        const content = button.nextElementSibling;
+        const isOpen = content.classList.contains("show");
+
+        // いったん全て閉じる（同時開閉を防ぐ場合）
+        document.querySelectorAll(".content").forEach(c => c.classList.remove("show"));
+        document.querySelectorAll(".folder-button").forEach(b => b.classList.remove("active"));
+
+        // 今押したものをトグル
+        if (!isOpen) {
+        button.classList.add("active");
+        content.classList.add("show");
+        }
+    });
+});
+
+const searchInputs = document.querySelectorAll("input");
+
+searchInputs.forEach(input => {input.placeholder = "おっと、まだ検索できないようだ";});
+
+document.querySelectorAll("main").forEach(box => {
+    const section = box.querySelectorAll("section");
+    if (section.length === 1) {
+        box.insertAdjacentHTML("afterbegin",
+            `<section>
+                <h3>おっと！</h3>
+                <p>まだこのページは完成していないようだ。<br>ごめんね。</p>
+                </section>`);
+    }
+});
